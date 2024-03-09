de153310640ab4e632fdd51e6eab84f440459899
ManuelMaisano <manumaisano@gmail.com>
2024-03-09 18:21:58 +0100

    add variable and consolelog

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = prompt("Inserisci il tuo nome");
+
+console.log(name);
+
+
+let username = prompt("Inserisci il tuo cognome");
+
+console.log(username);
+
+let favoritecolor = prompt("inserisci il tuo colore preferito");
+
+console.log(favoritecolor);
+
+
+
+
+
